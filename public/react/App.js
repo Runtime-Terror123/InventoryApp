bda521dbe3984b462d65dd9c4ac0b407b13bf7fd
Scheltema <thomas.scheltema@hp.com>
2025-03-24 15:57:29 -0500

    fix for file paths and header

--- public/react/App.js
+++ public/react/App.js
@@ -6,6 +6,7 @@ import Items from "./views/Items.jsx";
 
 // Prepend the API URL to any fetch calls.
 import apiURL from "./api.js";
+import AddItem from "./views/AddItem.jsx";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -18,6 +19,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddItem />} />
         <Route path="/items/:id" element={<Item />} />
         <Route path="/items" element={<Items />} />
       </Routes>
--- public/react/views/Home.jsx
+++ public/react/views/Home.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header.jsx";
 const Home = () => {
   return (
     <div>
-        <Header/>
+      <Header />
     </div>
   );
 };
