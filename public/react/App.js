db6194a01faf0a72084387a2fb1efa372d0bec8a
Dustin Alandzes <dustin.alandzes@hp.com>
2025-03-25 14:04:09 -0500

    remove/add header, remove unused variables and imports

--- public/react/App.js
+++ public/react/App.js
@@ -1,20 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home.jsx";
 import Item from "./views/Item.jsx";
 import Items from "./views/Items.jsx";
 
-// Prepend the API URL to any fetch calls.
-import apiURL from "./api.js";
 import AddItem from "./views/AddItem.jsx";
+import Header from "./components/Header";
 
 function App() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    // Fetch the items
-  }, []);
-
   return (
     <BrowserRouter>
       <Header />
