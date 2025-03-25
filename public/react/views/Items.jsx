8fc6fe1fd6d6ec7ad55eab40a7698eeba08d6790
briprogramming <110307766+briprogramming@users.noreply.github.com>
2025-03-25 14:54:19 -0500

    All Items Fetch (#29)

--- public/react/views/Items.jsx
+++ public/react/views/Items.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
 
 const Items = () => {
+  const [items, setItems]= useState();
+
+  useEffect(() => {
+    fetch ("https:/http://localhost:3000/api/items")
+    .then(response => response.json())
+    .then(data => setItems(data))
+    .catch(error => console.log('Error fetching items', error));
+  },
+[])
   return (
-    <div>
-      Items
-    </div>
-  )
-}
+    <div className= "items-container">
+     {items.map(item => (
+        <div className="item" key={item.id}>
+        <h3>{item.name}</h3>
+        <p>{item.description}</p>
+        <span>Price: ${item.price}</span>
+        <span>Category: {item.category}</span>
+      </div>
+    ))}
+  </div>
+);
+};
+
 
 export default Items
