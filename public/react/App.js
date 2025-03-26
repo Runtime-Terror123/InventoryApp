2fe438e21805212fc259708fe8a755fa43e89726
Scheltema <thomas.scheltema@hp.com>
2025-03-26 17:28:41 -0500

    add a cart and ability to place the order

--- public/react/App.js
+++ public/react/App.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home.jsx";
 import Item from "./views/Item.jsx";
 import Items from "./views/Items.jsx";
 import AddItem from "./views/AddItem.jsx";
 import EditItem from "./views/EditItem";
-
 import Header from "./components/Header";
+import { set } from "lodash";
 
 function App() {
+  const [isCartShown, setIsCartShown] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header setIsCartShown={setIsCartShown} />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/items/:id" element={<Item />} />
-        <Route path="/items" element={<Items />} />
+        <Route
+          path="/items"
+          element={
+            <Items
+              isCartShown={isCartShown}
+              setIsCartShown={setIsCartShown}
+              cartItems={cartItems}
+              setCartItems={setCartItems}
+            />
+          }
+        />
         <Route path="/add" element={<AddItem />} />
         <Route path="/edit/:id" element={<EditItem />} />
       </Routes>
--- public/react/components/Header.jsx
+++ public/react/components/Header.jsx
@@ -1,7 +1,12 @@
-import { NavLink } from "react-router-dom";
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
-export default function Header() {
+export default function Header({ setIsCartShown }) {
+  const toggleCart = () => {
+    setIsCartShown((prev) => !prev);
+  };
   return (
     <header
       id={"header"}
@@ -11,7 +16,7 @@ export default function Header() {
       }}
     >
       <h1>Inventory App</h1>
-      <nav>
+      <nav style={{ display: "flex", gap: 10, alignItems: "center" }}>
         <NavLink to="/" end>
           <h2>Home</h2>
         </NavLink>
@@ -21,6 +26,9 @@ export default function Header() {
         <NavLink to="/items" end>
           <h2>Items</h2>
         </NavLink>
+        <IconButton onClick={toggleCart}>
+          <ShoppingCartIcon />
+        </IconButton>
       </nav>
     </header>
   );
