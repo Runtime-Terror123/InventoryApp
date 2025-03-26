7a060d0b04097e33b90d52e209ac0b061efad420
Dustin Alandzes <5882512+DustinAlandzes@users.noreply.github.com>
2025-03-26 11:39:33 -0500

    add search (#41)
    * add thinking emoji face and allow decimals in price
    
    * add search
    
    * fix search import
    
    ---------
    
    Co-authored-by: Dustin Alandzes <dustin.alandzes@hp.com>

--- public/react/views/Home.jsx
+++ public/react/views/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Box } from "@mui/material";
+import Search from "../components/Search";
 
 const Home = () => {
   return (
     <Box>
-      Home page
+        <Search/>
     </Box>
   );
 };
