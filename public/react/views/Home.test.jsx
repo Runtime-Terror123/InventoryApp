c17cb606f55f1d6c19d3e37b93a7442eb5131412
Dustin Alandzes <fezf00@gmail.com>
2025-03-26 22:10:03 -0500

    a second test

--- public/react/views/Home.test.jsx
+++ public/react/views/Home.test.jsx
@@ -1,9 +1,13 @@
 import Home from "./Home";
 import React from 'react'
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 
 describe('Home.jsx', () => {
     it('renders without crashing', () => {
         render(<Home />);
+    })
+    it('has a search input', () => {
+        render(<Home />);
+        expect(screen.getByRole('textbox'))
     })
 })
