5a5b8d22a826cb2f415227ba7d7f858245e99e7f
Dustin Alandzes <dustin.alandzes@hp.com>
2025-03-27 10:50:47 -0500

    add server side validation

--- server/routes/items.js
+++ server/routes/items.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Item } = require("../models/index");
 const { Op } = require("sequelize");
+const {validationResult, matchedData, checkSchema} = require("express-validator");
 
 router.get("/", async (req, res) => {
   try {
@@ -26,9 +27,27 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+const validationSchema = {
+  name: { notEmpty: true },
+  description: { notEmpty: true },
+  image: {},
+  category: { notEmpty: true },
+  price: {
+    isLength: {
+      options: { min: 8 },
+    }
+  },
+}
+
+router.post("/", checkSchema(validationSchema), async (req, res) => {
   try {
-    const item = await Item.create(req.body);
+    const result = validationResult(req)
+    if(result.isEmpty() === false) {
+      return res.status(500).json( { errors: result.array() });
+    }
+    const data = matchedData(req)
+
+    const item = await Item.create(data);
     res.status(201).json(item);
   } catch (error) {
     console.error(error);
@@ -36,11 +55,17 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", checkSchema(validationSchema), async (req, res) => {
   try {
+    const result = validationResult(req)
+    if(result.isEmpty() === false) {
+      return res.status(500).json( { errors: result.array() });
+    }
+    const data = matchedData(req)
+
     const item = await Item.findByPk(req.params.id);
     if (item) {
-      await item.update(req.body);
+      await item.update(data);
       res.status(200).json(item);
     } else {
       res.status(404).json({ message: "Item not found" });
