e78fb2e8f2e877404b64715bd7414e3c6f6beadb
Dustin Alandzes <fezf00@gmail.com>
2025-03-27 02:50:54 -0500

    change styles for search

--- public/react/components/Search.jsx
+++ public/react/components/Search.jsx
@@ -51,17 +51,19 @@ export default function Search() {
             }}>
                 {error.message}
             </div>}
-            <form onSubmit={event => {
+            <form id={"search-form"} onSubmit={event => {
                 event.preventDefault();
                 debouncedFetch()
             }}>
-                <input type={"text"} placeholder={"Search..."} value={query}
+                <input type={"text"} placeholder={"Type here to search..."} value={query}
                        onChange={e => setQuery(e.target.value)}
                        style={{
+                           backgroundColor: "rgb(239, 239, 239, 0.3)",
                             borderRadius: '8px',
                             borderLeft: 'none',
                             borderRight: 'none',
                             borderTop: 'none',
+                            borderBottomWidth: '4px',
                             fontSize: '4em',
                             padding: '10px',
                         }}/>
