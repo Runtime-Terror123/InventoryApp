244819525e1abac01b6956f93fdf8316c887482e
Dustin Alandzes <dustin.alandzes@hp.com>
2025-03-27 11:07:48 -0500

    add test for app.js

---
+++ public/react/App.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {render, screen} from "@testing-library/react";
+import {describe, it, expect} from '@jest/globals';
+import App from "./App";
+import '@testing-library/jest-dom'
+
+describe('App.jsx', () => {
+    it('renders without crashing', async () => {
+        render(<App />);
+        expect(await screen.findByText("Inventory App")).toBeVisible();
+    })
+})
