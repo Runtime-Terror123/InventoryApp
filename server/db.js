722e18011dc60037aef496e268f45d73fce4b5e6
Scheltema <thomas.scheltema@hp.com>
2025-03-28 11:34:00 -0500

    fix db file

--- server/db.js
+++ server/db.js
@@ -3,10 +3,21 @@ const { Sequelize } = require("sequelize");
 
 let sequelize;
 
-sequelize = new Sequelize({
-  dialect: "sqlite",
-  logging: false,
-  storage: path.join(__dirname, "db.sqlite"),
-});
+if (process.env.NODE_ENV !== "production") {
+  sequelize = new Sequelize({
+    dialect: "sqlite",
+    logging: false,
+    storage: path.join(__dirname, "db.sqlite"),
+  });
+} else {
+  const password = process.env.DB_PASSWORD;
+  sequelize = new Sequelize(
+    `postgresql://inventoryappdatabase_nvc5_user:${password}@dpg-cvi8l22dbo4c73d916j0-a.oregon-postgres.render.com/inventoryappdatabase_nvc5?ssl=true`,
+    {
+      dialect: "postgres",
+      logging: false,
+    }
+  );
+}
 
 module.exports = sequelize;
